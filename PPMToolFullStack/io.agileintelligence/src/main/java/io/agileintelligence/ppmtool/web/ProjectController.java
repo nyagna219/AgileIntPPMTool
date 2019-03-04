380fc88a1f87704248bc8ecbc1fab2bb69b6f025
temp <temp@LP-5NWNZM2.GEO.CORP.HCL.IN>
2019-03-03 21:38:41 -0800

    Changed the status request to more readable format

--- PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -2,9 +2,13 @@ package io.agileintelligence.ppmtool.web;
 
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.services.ProjectService;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +23,10 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-        Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        if(result.hasErrors())
+        	return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+    	Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 }
