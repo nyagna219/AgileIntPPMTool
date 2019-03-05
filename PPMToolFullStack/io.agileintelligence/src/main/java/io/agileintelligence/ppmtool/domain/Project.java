a10f4e4fc84d328bcb4a7d27897f128e07650df4
temp <temp@LP-5NWNZM2.GEO.CORP.HCL.IN>
2019-03-05 15:54:16 -0800

    entity relationships project and backlog

--- PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,5 +1,6 @@
 package io.agileintelligence.ppmtool.domain;
 
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
@@ -10,26 +11,30 @@ import java.util.Date;
 @Entity
 public class Project {
 
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
-	    @NotBlank(message = "Project name is required")
-	    private String projectName;
-	    @NotBlank(message ="Project Identifier is required")
-	    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
-	    @Column(updatable = false, unique = true)
-	    private String projectIdentifier;
-	    @NotBlank(message = "Project description is required")
-	    private String description;
-	    @JsonFormat(pattern = "yyyy-mm-dd")
-	    private Date start_date;
-	    @JsonFormat(pattern = "yyyy-mm-dd")
-	    private Date end_date;
-	    @JsonFormat(pattern = "yyyy-mm-dd")
-	    @Column(updatable = false)
-	    private Date created_At;
-	    @JsonFormat(pattern = "yyyy-mm-dd")
-	    private Date updated_At;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Project name is required")
+    private String projectName;
+    @NotBlank(message ="Project Identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
+    private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
+    private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    @Column(updatable = false)
+    private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updated_At;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+    //
+    private Backlog backlog;
 
     public Project() {
     }
@@ -96,6 +101,14 @@ public class Project {
 
     public void setUpdated_At(Date updated_At) {
         this.updated_At = updated_At;
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @PrePersist
