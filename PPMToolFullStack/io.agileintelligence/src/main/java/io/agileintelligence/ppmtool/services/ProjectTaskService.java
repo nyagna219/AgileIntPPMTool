77cf814b130220c5bc1f5ec0e554651063b2b5b8
temp <temp@LP-5NWNZM2.GEO.CORP.HCL.IN>
2019-03-06 16:55:39 -0800

    update finish and delete operation

--- PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -10,6 +10,8 @@ import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectTaskService {
 
@@ -97,17 +99,17 @@ public class ProjectTaskService {
     }
 
     public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
-        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
 
         projectTask = updatedTask;
 
         return projectTaskRepository.save(projectTask);
     }
-    //Update project task
 
-    //find existing project task
 
-    //replace it with updated task
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+        projectTaskRepository.delete(projectTask);
+    }
 
-    //save update
 }
