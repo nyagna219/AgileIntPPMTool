c1dcca9be2b75edcecd6c7f996a1f8c8dc738e5e
temp <temp@LP-5NWNZM2.GEO.CORP.HCL.IN>
2019-03-08 12:10:08 -0800

    user specific create and get project tasks

--- PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/io.agileintelligence/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -25,13 +25,17 @@ public class ProjectTaskService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private ProjectService projectService;
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
 
-        try {
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask, String username){
+
+
             //PTs to be added to a specific project, project != null, BL exists
-            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            Backlog backlog =  projectService.findProjectByIdentifier(projectIdentifier, username).getBacklog(); //backlogRepository.findByProjectIdentifier(projectIdentifier);
             //set the bl to pt
+            System.out.println(backlog);
             projectTask.setBacklog(backlog);
             //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
             Integer BacklogSequence = backlog.getPTSequence();
@@ -51,24 +55,19 @@ public class ProjectTaskService {
                 projectTask.setStatus("TO_DO");
             }
 
-            if(projectTask.getPriority()== 0 || projectTask.getPriority()==null){ //In the future we need projectTask.getPriority()== 0 to handle the form
+            //Fix bug with priority in Spring Boot Server, needs to check null first
+            if(projectTask.getPriority()==null||projectTask.getPriority()==0){ //In the future we need projectTask.getPriority()== 0 to handle the form
                 projectTask.setPriority(3);
             }
 
             return projectTaskRepository.save(projectTask);
-        }catch (Exception e){
-            throw new ProjectNotFoundException("Project not Found");
-        }
+
 
     }
 
-    public Iterable<ProjectTask>findBacklogById(String id){
+    public Iterable<ProjectTask>findBacklogById(String id, String username){
 
-        Project project = projectRepository.findByProjectIdentifier(id);
-
-        if(project==null){
-            throw new ProjectNotFoundException("Project with ID: '"+id+"' does not exist");
-        }
+        projectService.findProjectByIdentifier(id, username);
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
